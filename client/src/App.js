42605d3e0880ea4b9091761b64ff6337eaae7e0d
Kiefer Johnson <kiefsjohn@gmail.com>
2024-01-08 17:54:43 -0800

    add api_key & front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log (`The count is ${count}`);
+
+    return () => {
+      console.log("I am being cleaned up");
+    }
+  }, [count]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{count}</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setCount(count - 1)}>
+          Decrement
+        </button>
+        <button onClick={() => setCount(count + 1)}>
+          Increment
+        </button>
       </header>
     </div>
   );
